6ab86620c96be7997a17f543616dbf83736e22ec
Fernando <steelco1326@gmail.com>
2021-12-02 12:49:19 -0600

    desarrollo paginas contacto y nosotros

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,46 @@ $('.slider').slick({
     pauseOnHover:false
 });
 
+
+
+$('.slider-nosotros .item').each(function() {
+  var slider = $(this);
+  slider.slick({
+    arrows: false,
+    dots: false,
+    accessibility: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    slidesToShow:4,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  var sLightbox = $(this);
+  sLightbox.slickLightbox({
+    src: 'src',
+    itemSelector: '.slide img',
+    caption:function(element, info) {
+    var span=$(element).parent().find(".desc");
+    return span.html(); }
+  });
+});
+
 $('.burguer').click(function () {
   $('.menu').toggleClass('active');
   $('.burguer').toggleClass('active');
